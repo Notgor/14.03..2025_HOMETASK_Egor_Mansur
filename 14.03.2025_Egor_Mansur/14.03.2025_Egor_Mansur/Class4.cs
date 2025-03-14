d4dfa9edc37e0927fee3628c5a8d787b05999d0f
Egor <egorkor88900@gmail.com>
2025-03-14 15:07:32 +0500

    10

---
+++ 14.03.2025_Egor_Mansur/14.03.2025_Egor_Mansur/Class4.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14._03._2025_Egor_Mansur
+{
+    class Class4
+    {
+    }
+}
